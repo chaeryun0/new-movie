c2296df00e4aa85c61275e42ca57f8736503eb9f
chaeryun0 <chaeryun00@gmail.com>
2023-07-26 01:35:53 +0900

    feat: SearchInput 컴포넌트 업데이트

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,15 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs'
+import { useState } from 'react';
 import styles from './SearchInput.module.css'
 
 const SearchInput = () => {
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    console.log(value);
+    setText(value.toLowerCase());
+  };
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(text ? `/search?q=${text}` : '/');
+  };
+
   return (
     <header className={styles.searchHeader}>
-      <form className={styles.searchForm}>
+      <form className={styles.searchForm} onSubmit={handleFormSubmit}>
         <BsSearch className={styles.searchIcon} />
         <input
-          type='search' 
+          type='search'
           placeholder='영화 제목을 입력해주세요.'
-          className={styles.searchInput} />
+          className={styles.searchInput}
+          onChange={handleInputChange}
+          value={text}
+        />
       </form>
     </header>
   )
