33fa9b8e3abd3274418fce6c466041424ec2f4e3
chaeryun0 <chaeryun00@gmail.com>
2023-07-29 00:35:46 +0900

    feat: 로컬스토리지 사용하여 데이터 저장

--- src/page/Favorite/Favorite.tsx
+++ src/page/Favorite/Favorite.tsx
@@ -1,18 +1,38 @@
-import { useState } from 'react';
 import MovieCard from '../../components/MovieCard/MovieCard';
 import { Movie } from '../../types/movie';
+import { Msg } from '../../assets/texts';
 import styles from './Favorite.module.css';
+import { useEffect, useState } from 'react';
 
 const Favorite = () => {
-  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
+  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>(() => {
+    const storedFavorites = localStorage.getItem('favoriteMovies');
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+  }, [favoriteMovies]);
+
+
+  const latestAddFavoreites = favoriteMovies.slice().reverse();
+
+  if (latestAddFavoreites.length === 0) {
+    return (
+      <section className={styles.favoriteList}>
+        <p className={styles.noFavorite}>{Msg.NO_FAVORITE}</p>
+      </section>
+    );
+  }
 
   return (
     <section className={styles.favoriteList}>
       <ul className={styles.favorites}>
-        {favoriteMovies?.map((movie) => (
-          <MovieCard 
-            key={movie.id} 
-            movie={movie} 
+        {latestAddFavoreites.map((movie, index) => (
+          <MovieCard
+            key={`${movie.id}-${index}`}
+            movie={movie}
+            isFavorite={true}
           />
         ))}
       </ul>
