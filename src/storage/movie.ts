25e9dec237336831834cb932dcc1f7cebe645285
chaeryun0 <chaeryun00@gmail.com>
2023-08-02 14:13:57 +0900

    feat: localstorage 관련 함수 모듈화

---
+++ src/storage/movie.ts
@@ -0,0 +1,31 @@
+import { Movie } from '../types/movie';
+
+const key = 'favoriteMovies';
+
+export const getFavoriteMovies = (): Movie[] => {
+  const storedFavorites = localStorage.getItem(key);
+  try {
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  } catch (error) {
+    console.error('Error parsing favorite movies from localStorage:', error);
+    return [];
+  }
+};
+
+export const addFavoriteMovie = (movie: Movie) => {
+  const arr = getFavoriteMovies();
+  const isAlreadyFavorite = arr.some((favMovie) => favMovie.id === movie.id);
+  if (!isAlreadyFavorite) {
+    arr.push(movie);
+    localStorage.setItem(key, JSON.stringify(arr));
+  }
+};
+
+export const removeFavoriteMovies = () => {
+  localStorage.removeItem(key);
+};
+
+export const setFavoriteMoviesInLocal = (movies: Movie[]) => {
+  const str = JSON.stringify(movies);
+  localStorage.setItem(key, str);
+};
